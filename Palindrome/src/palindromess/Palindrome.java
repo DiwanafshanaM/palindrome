c76ddf93585d2ca58b8f7a6ac785c7ffa3ce20ae
Acer <Acer@DESKTOP-F9ONNJE>
2022-02-13 20:27:17 +0530

    palindrome

---
+++ Palindrome/src/palindromess/Palindrome.java
@@ -0,0 +1,27 @@
+package palindromess;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		int num = 74756, reversedNum = 0, remainder;
+	    
+	    
+	    int originalNum = num;
+	    
+	    
+	    while (num != 0) {
+	      remainder = num % 10;
+	      reversedNum = reversedNum * 10 + remainder;
+	      num /= 10;
+	    }
+	    
+	 
+	    if (originalNum == reversedNum) {
+	      System.out.println(originalNum + " is Palindrome.");
+	    }
+	    else {
+	      System.out.println(originalNum + " is not Palindrome.");
+	    }
+	  }
+	}
+	
